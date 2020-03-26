4cc4ded0d1444651ed1d01e61623d848ffb5ff62
邹强 <719160804@qq.com>
2020-03-26 19:59:44 +0800

    查看所有GUIStyle

---
+++ ZQFramework/Assets/ZQFramwork/Tool/Editor/ShowAllGUIStyle.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ShowAllGUIStyle : EditorWindow
+{
+    private Vector2 scrollVector2 = Vector2.zero;
+
+
+    [MenuItem("ZQ/工具/查看所有GUIStyle", false)]
+    static void OpenWindow()
+    {
+        EditorWindow window = GetWindow(typeof(ShowAllGUIStyle));
+        window.minSize = new Vector2(300, 900);
+    }
+
+    string search = string.Empty;
+
+    void OnGUI()
+    {
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+
+        search = EditorGUILayout.TextField("", search, "ToolbarSeachTextField");
+
+        if (GUILayout.Button("", "ToolbarSeachCancelButton"))
+        {
+            search = string.Empty;
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.Space();
+
+
+
+
+
+
+        scrollVector2 = GUILayout.BeginScrollView(scrollVector2);
+
+        foreach (GUIStyle style in GUI.skin.customStyles)
+        {
+            if (search == string.Empty || style.name.Contains(search))
+            {
+                DrawStyleItem(style);
+            }
+
+        }
+
+        GUILayout.EndScrollView();
+    }
+
+    void DrawStyleItem(GUIStyle style)
+    {
+        EditorGUILayout.BeginVertical("box");
+
+        EditorGUILayout.SelectableLabel(style.name);
+
+        GUILayout.Button("", style);
+
+        EditorGUILayout.EndVertical();
+    }
+}
