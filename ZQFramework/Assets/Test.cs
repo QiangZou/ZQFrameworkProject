862e977d20fa0fc470acd00296e779f0730de6fc
邹强 <719160804@qq.com>
2020-03-30 18:47:30 +0800

    时间工具类

---
+++ ZQFramework/Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZQFramwork;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(TimeUtil.GetDateTimeNow().ToString());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ZQFramework/Assets/ZQFramwork/Utils/TimeUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZQFramwork
+{
+    /// <summary>
+    /// 时间工具
+    /// </summary>
+    public static class TimeUtil
+    {
+        public static DateTime GetDateTimeNow()
+        {
+            return DateTime.Now;
+        }
+        
+    }
+}
+
