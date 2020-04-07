2786c1e1eb09f3043225eddced3b4cece2457f7c
邹强 <719160804@qq.com>
2020-04-07 23:51:08 +0800

    Update CheckIllegalFileNamesTool.cs

--- ZQFramework/Assets/ZQFramwork/Tool/Editor/CheckIllegalFileNamesTool.cs
+++ ZQFramework/Assets/ZQFramwork/Tool/Editor/CheckIllegalFileNamesTool.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -15,11 +16,11 @@ namespace ZQFramwork
             {
                 Debug.Log(allFilePaths[i]);
 
-                if (IsIllegal(allFilePaths[i]) == false)
+                if (IsChineseLetter(allFilePaths[i]))
                 {
                     Debug.LogError(allFilePaths[i]);
                 }
-                
+
             }
         }
 
@@ -30,15 +31,30 @@ namespace ZQFramwork
                 char c = str[i];
 
                 //字符不属于字母和数字
-                //if (char.IsLetter(c) == false || char.IsDigit(c) == false)
-                //{
-                //    return false;
-                //}
+                if (char.IsLetter(c) == false || char.IsDigit(c) == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
+        static bool IsChineseLetter(string input)
+        {
+            int chfrom = Convert.ToInt32("4e00", 16); //范围（0x4e00～0x9fff）转换成int（chfrom～chend）
+            int chend = Convert.ToInt32("9fff", 16);
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                int code = Char.ConvertToUtf32(input, i);
+
+                if (code >= chfrom && code <= chend)
+                {
+                    return true; //当code在中文范围内返回true
+                }
             }
 
-            return true;
+            return false;
         }
     }
 }
