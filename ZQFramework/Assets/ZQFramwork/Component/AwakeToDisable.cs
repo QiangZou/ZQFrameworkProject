9020bf7028f28727c4e349f3c5febf2cf55751ec
邹强 <719160804@qq.com>
2020-10-02 00:30:56 +0800

    Awake执行后立刻Disable

---
+++ ZQFramework/Assets/ZQFramwork/Component/AwakeToDisable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Collections;
+#if UNITY_EDITOR	
+using UnityEditor;
+#endif
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(AwakeToDisable))]
+public class AwakeToDisableEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.HelpBox("Awake执行后立刻Disable\n\n使用场景\n1.被克隆的对象\n2.默认需要隐藏的对象", MessageType.Info);
+    }
+}
+#endif
+public class AwakeToDisable : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (gameObject.activeSelf == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
