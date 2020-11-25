fff3530f5007bbd6496ee36aef50ad9b6e131b89
邹强 <719160804@qq.com>
2020-11-25 21:48:25 +0800

    1

---
+++ ZQFramework/Assets/ZQFramwork/Extensions/Net/ExtensionsString.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ZQFramwork
+{
+    public static class ExtensionsString
+    {
+        /// <summary>
+        /// 获取数字
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static int GetNumber(this string self)
+        {
+            return int.Parse(System.Text.RegularExpressions.Regex.Replace(self, @"[^0-9]+", ""));
+        }
+
+       
+    }
+}
